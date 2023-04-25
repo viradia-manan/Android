48f5afba543dfebf142228c96f9b1eb243bc169b
viradia-manan <viradiamanan@gmail.com>
2023-04-25 14:27:52 +0530

    25-04-2023

--- Android Studio/Assignment/Android Fundamental/FragmentLifeCycle/app/src/main/java/com/example/fragmentlifecycle/MainActivity.kt
+++ Android Studio/Assignment/Android Fundamental/FragmentLifeCycle/app/src/main/java/com/example/fragmentlifecycle/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.fragmentlifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-
 }
--- Android Studio/Assignment/Android Fundamental/FragmentLifeCycle/app/src/main/java/com/example/fragmentlifecycle/lifecycle.kt
+++ Android Studio/Assignment/Android Fundamental/FragmentLifeCycle/app/src/main/java/com/example/fragmentlifecycle/lifecycle.kt
@@ -1,41 +1,41 @@
 package com.example.fragmentlifecycle
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 
 class lifecycle : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lifecycle, container, false)
+        var view= inflater.inflate(R.layout.fragment_lifecycle, container, false)
+
+        return view
     }
 
-    @SuppressLint("MissingSuperCall")
     override fun onStart() {
+        super.onStart()
         Toast.makeText(activity,"Start",Toast.LENGTH_LONG).show()
     }
 
-    @SuppressLint("MissingSuperCall")
     override fun onResume() {
+        super.onResume()
         Toast.makeText(activity,"Resume",Toast.LENGTH_LONG).show()
     }
 
-    @SuppressLint("MissingSuperCall")
     override fun onPause() {
+        super.onPause()
         Toast.makeText(activity,"Pause",Toast.LENGTH_LONG).show()
     }
 
-    @SuppressLint("MissingSuperCall")
-    override fun onStop() {
-        Toast.makeText(activity,"Stop",Toast.LENGTH_LONG).show()
-    }
-
-    @SuppressLint("MissingSuperCall")
     override fun onDestroy() {
+        super.onDestroy()
         Toast.makeText(activity,"Destroy",Toast.LENGTH_LONG).show()
     }
 
