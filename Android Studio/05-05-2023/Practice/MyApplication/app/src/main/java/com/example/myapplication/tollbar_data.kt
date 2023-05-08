09a5e4b5cc459f267a9ffcbd445389494846c8d2
viradia-manan <viradiamanan@gmail.com>
2023-05-08 09:06:01 +0530

    05-05-2023

---
+++ Android Studio/05-05-2023/Practice/MyApplication/app/src/main/java/com/example/myapplication/tollbar_data.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tollbar_data : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tollbar_data)
+    }
+}
