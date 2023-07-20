413e99bcdf65193873b583e49b2ee622eacaf2d2
viradia-manan <viradiamanan@gmail.com>
2023-07-20 22:58:02 +0530

    20-07-2023

--- Android Studio/Project/ArtGallery/app/src/main/java/com/example/artgallery/MainActivity.kt
+++ Android Studio/Project/ArtGallery/app/src/main/java/com/example/artgallery/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.artgallery
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.artgallery.databinding.ActivityMainBinding
+import com.example.artgallery.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,18 +20,16 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(HomeFragment())
 
-        val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    }
+
+    private fun replaceFragment(fragment: Fragment): Boolean {
+        val fm = supportFragmentManager
+        val ft = fm.beginTransaction()
+        ft.replace(R.id.frm_layout,fragment).commit()
+
+        return true
     }
 }
